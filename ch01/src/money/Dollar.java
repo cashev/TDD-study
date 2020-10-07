5cad250a0f2874b326d6e5c46682cc3711649760
cashew <42924021+cashev@users.noreply.github.com>
2020-10-07 23:30:47 +0900

    currencyをコンストラクタで設定

--- ch01/src/money/Dollar.java
+++ ch01/src/money/Dollar.java
@@ -2,12 +2,12 @@ package money;
 
 class Dollar extends Money {
 
-  Dollar(int amount) {
+  Dollar(int amount, String currency) {
     this.amount = amount;
-    currency = "USD";
+    this.currency = currency;
   }
 
   Money times(int multiplier) {
-    return new Dollar(amount * multiplier);
+    return new Dollar(amount * multiplier, null);
   }
 }
